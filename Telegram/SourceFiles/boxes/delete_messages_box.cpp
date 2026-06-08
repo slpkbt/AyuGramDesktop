6855d516729f8d2a2a0e716b3a73a9ceb5ccf193
Anton <45075347+slpkbt@users.noreply.github.com>
2026-06-08 04:48:45 +0300

    fix: unbreak linux ci after telegram merge

--- Telegram/SourceFiles/boxes/delete_messages_box.cpp
+++ Telegram/SourceFiles/boxes/delete_messages_box.cpp
@@ -38,22 +38,6 @@ DeleteMessagesBox::DeleteMessagesBox(
 	not_null<HistoryItem*> item)
 : _session(&item->history()->session())
 , _ids(1, item->fullId()) {
-	const auto peer = item->history()->peer;
-	const auto channel = peer->asChannel();
-	if (suggestModerateActions) {
-		_moderateBan = item->suggestBanReport();
-		_moderateDeleteAll = item->suggestDeleteAllReport();
-	} else if (item->out()) {
-		const auto chat = peer->asChat();
-		if ((chat && chat->canDeleteMessages()) ||
-			(channel && !channel->isBroadcast() && channel->canDeleteMessages())) {
-			_moderateDeleteAll = true;
-		}
-	}
-	if ((_moderateBan || _moderateDeleteAll) && channel) {
-		_moderateFrom = item->from();
-		_moderateInChannel = channel;
-	}
 }
 
 DeleteMessagesBox::DeleteMessagesBox(
